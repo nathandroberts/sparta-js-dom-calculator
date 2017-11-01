8c2d9fcf68392d34e09bf9807b743b6b96ec62af
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-01 22:18:22 +0000

    attempted to increase a value after a number button is pressed to distinguish between press one and press 2

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -4,9 +4,12 @@ var allButtons = document.getElementsByClassName('buttonNum');
 function numberButtonFunction() {
   for (var i = 0; i < allButtons.length; i++) {
     var button = allButtons[i];
+    var timesPressed = 1;
     button.addEventListener('click', function(event){
       console.log(this.value + ' was clicked');
       document.getElementById('screen').innerHTML = this.value;
+      timesPressed++;
+      return this.value;
     })
   }
 }
@@ -19,8 +22,46 @@ function operatorButtonFunction() {
     operator.addEventListener('click', function(event){
       console.log(this.value + ' was clicked');
       document.getElementById('screen').innerHTML = this.value;
+      var operatorType = this.value;
+      return operatorType;
     })
+
   }
 }
- numberButtonFunction();
- operatorButtonFunction();
+function equalsButtonFunction() {
+  var equals = document.getElementsByClassName('equals')
+  equals[0].addEventListener('click', function (event) {
+
+    var solution = equalsCalculation();
+    document.getElementById('screen').innerHTML = solution;
+  })
+}
+
+function equalsCalculation() {
+  var solution;
+  input1 =
+  operatorType = operatorButtonFunction();
+  if (operatorType === '+') {
+    solution =input1 + input2;
+    return solution;
+  } else if (operatorType === '-') {
+    solution =input1 - input2;
+    return solution;
+  } else if (operatorType === '*') {
+    solution =input1 * input2;
+    return solution;
+  } else if (operatorType === '/') {
+    solution =input1 / input2;
+    return solution;
+  }
+}
+
+function numberIncrement() {
+
+}
+equalsButtonFunction()
+
+
+numberButtonFunction();
+operatorButtonFunction();
+// equalsButtonFunction(numberButtonFunction(), operatorButtonFunction(), numberButtonFunction())
