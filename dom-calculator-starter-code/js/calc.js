0337bcc97cc1431270d0e5496c8dcf6355b2aa0b
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-01 21:13:46 +0000

    simple operator buttons added

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -6,8 +6,21 @@ function numberButtonFunction() {
     var button = allButtons[i];
     button.addEventListener('click', function(event){
       console.log(this.value + ' was clicked');
+      document.getElementById('screen').innerHTML = this.value;
     })
   }
-
 }
-buttonLinker();
+var allOperators = document.getElementsByClassName('operator');
+
+
+function operatorButtonFunction() {
+   for (var i = 0; i < allOperators.length; i++) {
+    var operator = allOperators[i]
+    operator.addEventListener('click', function(event){
+      console.log(this.value + ' was clicked');
+      document.getElementById('screen').innerHTML = this.value;
+    })
+  }
+}
+ numberButtonFunction();
+ operatorButtonFunction();
